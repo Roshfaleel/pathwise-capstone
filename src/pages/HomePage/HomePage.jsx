7338115d567fe5bd4a39f8fe600e115fc40504de
Roshani Faleel <roshfaleel98@gmail.com>
2024-11-24 03:40:37 -0500

    seperate features card component

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,9 +1,8 @@
 import React from "react";
 import "./HomePage.scss";
-import skillTracker from "../../assets/images/skill_tracking.jpg";
-import goal from "../../assets/images/goal.jpg";
-import dashboard from "../../assets/images/dashboard.jpg";
+
 import NavBar from "../../components/NavBar/Navbar";
+import FeaturesSection from "../../components/FeaturesSection/FeaturesSection";
 
 function HomePage() {
   return (
@@ -32,47 +31,7 @@ function HomePage() {
           "
         </p>
       </section>
-      <section className="features__container">
-        <h3 className="features__title">Features</h3>
-        <div className="features__cards">
-        <div className="features__card">
-          <img
-            className="features__img"
-            src={skillTracker}
-            alt="vector image of skill tracking"
-          />
-          <div className="features__card-content">
-            <h4 className="features__card-title">Skill & Achievement Tracking</h4>
-            <p className="features__card-content">
-              Log and showcase your skills with detailed progress and
-              achievements.
-            </p>
-          </div>
-        </div>
-        <div className="features__card">
-          <img
-            className="features__img"
-            src={goal}
-            alt="vector image of goal visualization"
-          />
-          <div className="features__card-content">
-            <h4 className="features__card-title">Goal Visualization</h4>
-            <p className="features__card-content">
-              Set, track, and visualize personal and educational milestones.
-            </p>
-          </div>
-        </div>
-        <div className="features__card">
-          <img className="features__img"
-            src={dashboard}
-            alt="vector image of goal visualization"/>
-          <div className="features__card-content">
-            <h4 className="features__card-title">Custom Dashboards</h4>
-            <p className="features__card-content">Monitor your journey with intuitive charts and insights.</p>
-          </div>
-        </div>
-        </div>
-      </section>
+      <FeaturesSection/>
     </>
   );
 }
