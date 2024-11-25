868789a2823542770eb2550c798c66f702de3cac
Roshani Faleel <roshfaleel98@gmail.com>
2024-11-25 00:25:40 -0500

    add the basic structure of the login page

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -1,11 +1,23 @@
-import React from 'react'
+import React from 'react';
+import './LoginPage.scss';
+import logo from "../../assets/images/PW_Logo.png";
 
-function LoginPage() {
+
+const LoginPage = () => {
   return (
-    <div>
-      Login Page
+    <div className="login-page">
+      <div className="login-page__background"></div>
+      <div className="login-page__card">
+        <img className='login-page__logo' src={logo}/>
+        <h2>Welcome Back to PathWise</h2>
+        <form>
+          <input type="text" placeholder="Username" />
+          <input type="password" placeholder="Password" />
+          <button type="submit">Login</button>
+        </form>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default LoginPage
+export default LoginPage;
