8b829a3d86daae61192cfb2049e8a3d864d193f0
Roshani Faleel <roshfaleel98@gmail.com>
2024-11-25 05:39:24 -0500

    add the API to create an account

--- src/pages/SignupPage/SignupPage.jsx
+++ src/pages/SignupPage/SignupPage.jsx
@@ -1,10 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import "./SignUpPage.scss";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import FormComponent from "../../components/FormComponent/FormComponent";
 import AuthCardLayout from "../../components/AuthCardLayout/AuthCardLayout";
+import axios from "axios";
 
 const SignUpPage = () => {
+  const navigate = useNavigate();
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+  const [error, setError] = useState(null)
+
   const signUpFields = [
     {
       label: "Name",
@@ -12,6 +22,7 @@ const SignUpPage = () => {
       name: "name",
       type: "text",
       placeholder: "Your full name",
+      value: formData.name,
     },
     {
       label: "Email",
@@ -19,6 +30,7 @@ const SignUpPage = () => {
       name: "email",
       type: "email",
       placeholder: "Your email address",
+      value : formData.email,
     },
     {
       label: "Password",
@@ -26,6 +38,7 @@ const SignUpPage = () => {
       name: "password",
       type: "password",
       placeholder: "Password",
+      value : formData.password,
     },
     {
       label: "Confirm Password",
@@ -33,20 +46,54 @@ const SignUpPage = () => {
       name: "confirmPassword",
       type: "password",
       placeholder: "Confirm your password",
+      value : formData.confirmPassword,
     },
   ];
 
-  const handleSignUp = (e) => {
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const API_URL = import.meta.env.VITE_API_URL;
+
+  const handleSignUp = async (e) => {
     e.preventDefault();
-    // Handle sign-up logic
+   
+    const formData = new FormData(e.target);
+    const userData = {};
+    formData.forEach((value, key) =>{
+      userData[key] =value;
+    });
+
+    if (userData.password !== userData.confirmPassword){
+      setError("Passwords do not match");
+      return;
+    }
+
+    setError (null);
+
+    try {
+      const response = await axios.post(`${API_URL}/api/users`, userData);
+  
+    if (response.status === 201){
+      navigate("/login");
+    }
+  } catch (error){
+    console.error("Error during sign-up", error);
+    setError(error.response?.data?.message || "An error occured")
+  }
   };
 
   return (
     <AuthCardLayout headerText="Join PathWise">
+      {error && <p className="error">{error}</p>}
       <FormComponent
         formFields={signUpFields}
         buttonText="Sign Up"
         onSubmit={handleSignUp}
+        onChange={handleChange}
+        value = {formData}
       />
       <p className="signup__link">
         Already have an account?
