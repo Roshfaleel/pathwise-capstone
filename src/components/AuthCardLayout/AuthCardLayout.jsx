c569b5bb1621b94fd0625913655804a5cdcf469d
Roshani Faleel <roshfaleel98@gmail.com>
2024-11-25 03:00:43 -0500

    add back arrow to nav back to home

--- src/components/AuthCardLayout/AuthCardLayout.jsx
+++ src/components/AuthCardLayout/AuthCardLayout.jsx
@@ -1,11 +1,22 @@
 import "./AuthCardLayout.scss";
 import logo from "../../assets/images/PW_Logo.png";
+import { useNavigate } from "react-router-dom";
+import arrowBack from "../../assets/icons/arrow_back-24px.svg"
 
 const AuthCardLayout = ({ children, headerText }) => {
+    const navigate = useNavigate();
+
   return (
     <div className="auth-layout">
       <div className="auth-layout__background"></div>
       <div className="auth-layout__card">
+      <button
+          className="auth-layout__back-button"
+          onClick={() => navigate("/")}
+          aria-label="Go back"
+        >
+          <img src={arrowBack} alt="back-arrow"/>
+        </button>
         <img className="auth-layout__logo" src={logo} alt="PathWise Logo" />
         <h2 className="auth-layout__header">{headerText}</h2>
         {children}
