a35ac2755f199279c10db14a775ac7bebb772d86
Roshani Faleel <roshfaleel98@gmail.com>
2024-11-27 11:17:06 -0500

    set the API to get the user details

--- src/pages/MyaccountPage/MyaccountPage.jsx
+++ src/pages/MyaccountPage/MyaccountPage.jsx
@@ -1,11 +1,35 @@
-import React from 'react'
+import "./MyaccountPage.scss";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { Card, Image } from "react-bootstrap";
 
 function MyaccountPage() {
+  const [userDetails, setUserDetails] = useState(null);
+
+  useEffect(() => {
+    const userId = localStorage.getItem("userId");
+    const API_URL = import.meta.env.VITE_API_URL;
+
+    const fetchUser = async () => {
+      try {
+        const userResponse = await axios.get(`${API_URL}/api/users/${userId}`);
+        const userData = userResponse.data;
+        setUserDetails(userData)
+      } catch (error) {
+        console.error("Error fetching user details:", error);
+      }
+    };
+    fetchUser();
+  }, []);
+  if (!userDetails) {
+    return <p>Loading...</p>;
+  }
+
   return (
-    <div>
-      Myaccount
+    <div className="account">
+      <h1>My Account</h1>      
     </div>
-  )
+  );
 }
 
-export default MyaccountPage
+export default MyaccountPage;
