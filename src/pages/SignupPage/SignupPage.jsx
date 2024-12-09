21274300e7f1151f112fd8fde9d7376f91f82afb
Roshani Faleel <roshfaleel98@gmail.com>
2024-12-09 08:07:40 -0500

    update sinup.scss directory on signup page

--- src/pages/SignupPage/SignupPage.jsx
+++ src/pages/SignupPage/SignupPage.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import "./SignUpPage.scss";
+import "./SignupPage.scss";
 import { Link, useNavigate } from "react-router-dom";
 import FormComponent from "../../components/FormComponent/FormComponent";
 import AuthCardLayout from "../../components/AuthCardLayout/AuthCardLayout";
